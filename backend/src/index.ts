694344c81ad9e0e1bad4aba7bc9d6a6ba125203e
Ron Cohen <ronc7500@gmail.com>
2025-09-18 20:53:46 +0300

    Fix CORS issues

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,22 +1,31 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 
 dotenv.config();
 
 const app = express();
 const { PORT } = process.env;
 
+app.use(cors({
+  origin: 'https://localhost:8080',
+  credentials: true
+}));
+
 if (!PORT) {
   throw new Error('PORT is not set in the environment (.env)');
 }
 
-// Middleware to parse JSON
 app.use(express.json());
 
-// Hello World route
 app.get('/', (req, res) => {
   res.json({ message: 'Hello World!' });
 });
+
+app.get('/health', (req, res) => {
+  res.status(200).json({ message: 'OK' });
+});
+
 
 // Start the server
 app.listen(PORT, () => {
